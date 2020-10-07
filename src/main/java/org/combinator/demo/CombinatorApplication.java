e4b2b54d66c390fa9f606258cd47166f53315e0a
sreekumar <sree@mailnator.com>
2020-10-07 19:20:27 +0530

    init project

---
+++ src/main/java/org/combinator/demo/CombinatorApplication.java
@@ -0,0 +1,15 @@
+package org.combinator.demo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CombinatorApplication {
+
+    public static final Logger logger = LoggerFactory.getLogger(CombinatorApplication.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("hello");
+    }
+
+}
